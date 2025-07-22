ea134ed129e43147861772e42c9d224612dbeadf
0xleal <francisco@talentprotocol.com>
2025-07-22 18:41:24 +0100

    Improve header (#15)
    * Add option to disable filling on active state
    
    * Make header hidden on small screens

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -6,12 +6,16 @@ import { useEffect } from "react";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      return;
+    }
+
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: "/ingest",
       ui_host: "https://eu.posthog.com",
       defaults: "2025-05-24",
-      capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-      debug: process.env.NODE_ENV === "development",
+      capture_exceptions: true,
+      debug: false,
     });
   }, []);
 
