3a9bcfb2c1b6a2a9a6e16cf3008fcd9a444cfec3
francisco-leal <francisco@talentprotocol.com>
2025-07-25 17:14:27 +0100

    Tweak content policies

--- next.config.mjs
+++ next.config.mjs
@@ -61,21 +61,21 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-              default-src 'self';
-              script-src 'self' https://challenges.cloudflare.com;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: blob:;
-              font-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://use.typekit.net https://p.typekit.net;
+              font-src 'self' https://fonts.gstatic.com https://use.typekit.net https://p.typekit.net;
               object-src 'none';
               base-uri 'self';
               form-action 'self';
               frame-ancestors 'none';
               child-src https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org;
               frame-src https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org https://challenges.cloudflare.com;
-              connect-src 'self' https://auth.privy.io wss://relay.walletconnect.com wss://relay.walletconnect.org wss://www.walletlink.org https://*.rpc.privy.systems https://explorer-api.walletconnect.com;
+              connect-src 'self' https://auth.privy.io wss://relay.walletconnect.com wss://relay.walletconnect.org wss://www.walletlink.org https://*.rpc.privy.systems https://explorer-api.walletconnect.com https://pulse.walletconnect.org https://api.web3modal.org;
               worker-src 'self';
               manifest-src 'self'
-            `,
+            `
+              .replace(/\s+/g, " ")
+              .trim(),
           },
         ],
       },
