196e102c4e4e3fd0e527d7c01a80208deec681f0
francisco-leal <francisco@talentprotocol.com>
2025-07-28 16:53:28 +0100

    Remove unused import

--- components/common/WelcomeModalHandler.tsx
+++ components/common/WelcomeModalHandler.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
 import { ShareCreatorScoreModal } from "@/components/modals/ShareCreatorScoreModal";
 import { useShareCreatorScore } from "@/hooks/useShareCreatorScore";
 
 export function WelcomeModalHandler() {
-  const { isOpen, onOpenChange, hasSeenWelcome } = useShareCreatorScore();
+  const { isOpen, onOpenChange } = useShareCreatorScore();
 
   return <ShareCreatorScoreModal open={isOpen} onOpenChange={onOpenChange} />;
 }
